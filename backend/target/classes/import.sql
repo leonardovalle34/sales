64315b064051f03333b3f6db493ec1d4bfa4ab3b
Leonardo do Valle <107518514+leonardovalle34@users.noreply.github.com>
2024-08-18 00:29:02 -0300

    Add files via upload
    backend up

---
+++ backend/target/classes/import.sql
@@ -0,0 +1,6 @@
+-- Dados para agosto de 2024
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos Oliveira', 15, 5, 2000.0, '2024-07-31');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Fernanda Costa', 18, 5, 2000.0, '2024-07-31');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Carlos Almeida', 13, 3, 1200.0, '2024-07-31');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Roberta Ribas', 14, 4, 1600.0, '2024-07-31');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos krambeck', 14, 3, 1750.0, '2024-07-31');
